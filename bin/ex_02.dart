20fc155dc57ee6c1f0fb18106e64428e30cfaf38
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-18 11:48:09 -0400

    add doc

--- bin/ex_02.dart
+++ bin/ex_02.dart
@@ -1,29 +1,27 @@
 void main() {
-  var x=5;
-  var y=7;
-  var z=8;
-  var o=x-y/z;
-  print('1. x-y/z=${o}');
-//They are the same
-  
-  var pi=3.14;
-  var r=10;
-  var area=pi*r*r;
-  print('2. circle area=${area}'); 
-  
-  var sqft_acres=0.0000229568411;
-  var sqft=220*260;
-  var acres=sqft*sqft_acres; 
-  print('3. acres=${acres}'); 
-  
-  var time=9.58;
-  var dist=100;
-  var dist_kilom=0.1;
-  var time_hour=9.58/3600; 
-  var speed=dist_kilom/time_hour;
-  print('4. speed=${speed} km/h');
-  
-  
+  var x = 5;
+  var y = 7;
+  var z = 8;
+  var o = x - y / z;
+  print('1. x-y/z = ${o}');
+  //They are the same
+
+  var pi = 3.14;
+  var r = 10;
+  var area = pi * r * r;
+  print('2. circle area = ${area}');
+
+  var sqftAcres = 0.0000229568411;
+  var sqft = 220 * 260;
+  var acres = sqft * sqftAcres;
+  print('3. acres = ${acres}');
+
+  var time = 9.58;
+  var dist = 100;
+  var distKilom = 0.1;
+  var time_hour = 9.58/3600;
+  var speed = distKilom/time_hour;
+  print('4. speed = ${speed} km/h');
 }
 
 
